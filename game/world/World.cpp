359ccf76d3180c6085dfbefdea14fffe7ffcc407
Jared Nichols <Jararered@outlook.com>
2022-04-26 09:50:28 -0400

    surrounding chunks now update when needed

--- game/world/World.cpp
+++ game/world/World.cpp
@@ -97,6 +97,28 @@ void World::SetBlock(glm::ivec3 worldPos, Block blockToSet)
 
         Renderer::UpdateMeshInQueue(index);
 
+        // Updating neighboring meshes in the world
+        if (chunkCoord.x == 0)
+        {
+            Instance().m_ChunkDataMap.at({index.x - 1, index.y, index.z})->GenerateMesh();
+            Renderer::UpdateMeshInQueue({index.x - 1, index.y, index.z});
+        }
+        if (chunkCoord.x == 15)
+        {
+            Instance().m_ChunkDataMap.at({index.x + 1, index.y, index.z})->GenerateMesh();
+            Renderer::UpdateMeshInQueue({index.x + 1, index.y, index.z});
+        }
+        if (chunkCoord.z == 0)
+        {
+            Instance().m_ChunkDataMap.at({index.x, index.y, index.z - 1})->GenerateMesh();
+            Renderer::UpdateMeshInQueue({index.x, index.y, index.z - 1});
+        }
+        if (chunkCoord.z == 15)
+        {
+            Instance().m_ChunkDataMap.at({index.x, index.y, index.z + 1})->GenerateMesh();
+            Renderer::UpdateMeshInQueue({index.x, index.y, index.z + 1});
+        }
+
         return;
     }
     else
