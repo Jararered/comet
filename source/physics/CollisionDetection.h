99088196c2792f8139169a275f1c7d1e5c4562a9
Jared Nichols <Jararered@outlook.com>
2022-04-27 10:35:08 -0400

    removed unneeded functions

--- source/physics/CollisionDetection.h
+++ source/physics/CollisionDetection.h
@@ -22,49 +22,11 @@ struct Collision
         };
     }
 
+    // Returns true if the collision boxes intersect at all
     static bool Intersect(Collision a, Collision b)
     {
         return (a.NegativeX < b.PositiveX && a.PositiveX > b.NegativeX) &&
                (a.NegativeY < b.PositiveY && a.PositiveY > b.NegativeY) &&
                (a.NegativeZ < b.PositiveZ && a.PositiveZ > b.NegativeZ);
-    }
-
-    static void FixX(glm::vec3 &position, Collision a, Collision b)
-    {
-        // X
-        if (a.NegativeX < b.PositiveX)
-        {
-            position.x += b.PositiveX - a.NegativeX;
-        }
-        else if (a.PositiveX > b.NegativeX)
-        {
-            position.x -= a.PositiveX - b.NegativeX;
-        }
-    }
-
-    static void FixY(glm::vec3 &position, Collision a, Collision b)
-    {
-        // Y
-        if (a.NegativeY < b.PositiveY)
-        {
-            position.y += b.PositiveY - a.NegativeY;
-        }
-        else if (a.PositiveY > b.NegativeY)
-        {
-            position.y -= a.PositiveY - b.NegativeY;
-        }
-    }
-
-    static void FixZ(glm::vec3 &position, Collision a, Collision b)
-    {
-        // Z
-        if (a.NegativeZ < b.PositiveZ)
-        {
-            position.z += b.PositiveZ - a.NegativeZ;
-        }
-        else if (a.PositiveZ > b.NegativeZ)
-        {
-            position.z -= a.PositiveZ - b.NegativeZ;
-        }
     }
 };
