294b2fe2c717981879a9e036e55e9c4f1dbe35bb
Jared Nichols <Jararered@outlook.com>
2022-04-27 06:37:37 -0400

    moved definitions to cpp file

--- game/interfaces/RenderInterface.h
+++ game/interfaces/RenderInterface.h
@@ -13,90 +13,10 @@
 class RenderInterface : public Interface
 {
   public:
-    RenderInterface()
-    {
-        InterfaceHandler::AddInterface(this);
-        m_RenderDistance = World::RenderDistance();
-        m_RenderShape = World::RenderShape();
-        m_WorldSeed = ChunkGenerator::Seed();
-        m_OverlayColor = Renderer::OverlayColor();
-        m_BackgroundColor = Renderer::BackgroundColor();
-    }
-    ~RenderInterface() {}
+    RenderInterface();
+    ~RenderInterface();
 
-    void Draw() override
-    {
-        ImGui::Begin("Render Menu");
-
-        if (ImGui::Button("Play"))
-        {
-            MouseHandler::CaptureCursor();
-        }
-        ImGui::SameLine();
-        if (ImGui::Button("Reset Renderer"))
-        {
-            Renderer::ResetRenderer();
-            World::Finalize();
-            Renderer::SetResetting(false);
-
-            World::Initialize();
-            World::InitializeThread();
-        }
-        ImGui::SameLine();
-        if (ImGui::Button("Exit"))
-        {
-            Engine::SetShouldClose(true);
-        }
-
-        ImGui::Text("Render Modes: ");
-        ImGui::SameLine();
-        if (ImGui::Button("Wireframe"))
-        {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        }
-        ImGui::SameLine();
-
-        if (ImGui::Button("Filled"))
-        {
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        }
-
-        if (ImGui::SliderInt("Render Distance", &m_RenderDistance, 0, 16))
-        {
-            World::SetRenderDistance(m_RenderDistance);
-        }
-
-        ImGui::Text("Render Shape: ");
-        ImGui::SameLine();
-        if (ImGui::Button("Square"))
-        {
-            World::SetRenderShape(RenderShape::Square);
-        }
-        ImGui::SameLine();
-        if (ImGui::Button("Circle"))
-        {
-            World::SetRenderShape(RenderShape::Circle);
-        }
-
-        if (ImGui::InputInt("World Seed", &m_WorldSeed))
-        {
-            ChunkGenerator::SetSeed(m_WorldSeed);
-        }
-
-        if (ImGui::SliderFloat3("Overlay Color", &m_OverlayColor[0], -1.0f, 1.0f))
-        {
-            Renderer::SetOverlayColor(m_OverlayColor);
-        }
-        if (ImGui::SliderFloat3("Background Color", &m_BackgroundColor[0], 0.0f, 1.0f))
-        {
-            Renderer::SetBackgroundColor(m_BackgroundColor);
-        }
-
-        ImGui::Text("Performance: %.1f FPS", ImGui::GetIO().Framerate);
-        ImGui::Text("Draws/Frame: %d", Renderer::DrawCallsPerFrame());
-
-        ImGui::End();
-    }
+    void Draw() override;
 
   private:
     int m_RenderDistance;
