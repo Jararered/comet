f0a26ca63b82524789baea9259ddef1254f0a54d
Jared Nichols <Jararered@outlook.com>
2022-05-18 08:10:16 -0400

    added constructor for no move/copy ops

--- source/render/Vertex.h
+++ source/render/Vertex.h
@@ -4,6 +4,7 @@
 
 struct Vertex
 {
+    Vertex(glm::vec3 position, glm::vec2 textureCoordinate, glm::vec3 normal) : Position(position), TextureCoordinate(textureCoordinate), Normal(normal) {}
     glm::vec3 Position;
     glm::vec2 TextureCoordinate;
     glm::vec3 Normal;
