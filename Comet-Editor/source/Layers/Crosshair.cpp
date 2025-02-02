82427c14332c4d5f50bbc42817bd7029239f80e1
Jararered <Jararered@icloud.com>
2025-02-02 11:21:42 -0500

    refactor Crosshair and Mesh classes: enhance code readability with additional comments and improve buffer initialization logic

--- Comet-Editor/source/Layers/Crosshair.cpp
+++ Comet-Editor/source/Layers/Crosshair.cpp
@@ -16,22 +16,30 @@ Crosshair::Crosshair()
     std::string texturePath = path.string() + "/Resources/Textures/crosshair.png";
     m_Texture.Create(texturePath);
 
+    // Create the indices for the crosshair mesh
     m_Indices.insert(m_Indices.end(), {0, 1, 2, 2, 3, 0});
+
+    // Create the vertices for the crosshair mesh
     m_Vertices.insert(m_Vertices.end(), {Vertex{{-0.0225f, -0.04f, 0.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}}, Vertex{{0.0225f, -0.04f, 0.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, 0.0f}},
                                          Vertex{{0.0225f, 0.04f, 0.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 0.0f}}, Vertex{{-0.0225f, 0.04f, 0.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, 0.0f}}});
 
-    // m_Mesh = Mesh(&m_Vertices, &m_Indices, &m_Shader);
+    // Create the crosshair mesh
     m_Mesh = Mesh(&m_Vertices, &m_Indices, &m_Shader);
+
+    // Initialize the crosshair mesh
     m_Mesh.Initialize();
 }
 
 void Crosshair::Draw()
 {
+    // Bind the shader, texture, and mesh
     m_Texture.Bind();
     m_Mesh.Bind();
 
+    // Set the shader uniforms
     glUniform1i(glGetUniformLocation(m_Shader.GetID(), "u_Texture"), 0);
 
+    // Draw the mesh
     glEnable(GL_BLEND);
     glDrawElements(GL_TRIANGLES, m_Mesh.GetIndices()->size(), GL_UNSIGNED_INT, (void*)0);
     glDisable(GL_BLEND);
--- Comet/source/Renderer/Mesh.cpp
+++ Comet/source/Renderer/Mesh.cpp
@@ -21,20 +21,25 @@ void Mesh::Unbind()
 
 void Mesh::Initialize()
 {
+    // Generating buffers
     glGenVertexArrays(1, &m_VAO);
     glGenBuffers(1, &m_VBO);
     glGenBuffers(1, &m_IBO);
 
+    // Binding
     glBindVertexArray(m_VAO);
 
+    // Buffer geometry data
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
     glBufferData(GL_ARRAY_BUFFER, 2 * p_Vertices->size() * sizeof(Vertex), (void*)0, GL_DYNAMIC_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, p_Vertices->size() * sizeof(Vertex), p_Vertices->data());
 
+    // Buffer index data
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * p_Indices->size() * sizeof(unsigned int), (void*)0, GL_DYNAMIC_DRAW);
     glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, p_Indices->size() * sizeof(unsigned int), p_Indices->data());
 
+    // Setting vertex attributes
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, Position)));
     glEnableVertexAttribArray(1);
@@ -42,6 +47,7 @@ void Mesh::Initialize()
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, Normal)));
 
+    // Unbinding everything
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
